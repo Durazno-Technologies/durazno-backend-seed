e15fd4a9c21726c6d58fb16a5ee850c53dce0425
Marco Dali <70110654+marcodali@users.noreply.github.com>
2023-01-04 12:28:02 -0600

    Update index.ts

--- src/database/services/index.ts
+++ src/database/services/index.ts
@@ -1,6 +1,9 @@
 import createDynamoDBClient from "../db";
 import UserService from "./userService";
 
-const { USERS_TABLE } = process.env;
+// do not edit, managed by @durazno-technologies/create-dzn package
+const {
+  USERS_TABLE
+} = process.env;
 
 export const userService = new UserService(createDynamoDBClient(), USERS_TABLE);
